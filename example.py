38d644cea18e002e0afe5bcc175aba611d7b3ef0
wenchi <b99901053@gmail.com>
2018-02-12 16:21:09 -0800

    Add Code.

--- example.py
+++ example.py
@@ -18,17 +18,17 @@ documents = ["Human machine interface for lab abc computer applications",
 stoplist = set('for a of the and to in'.split())
 texts = [[word for word in document.lower().split() if word not in stoplist]
          for document in documents]
->>>
+
 # remove words that appear only once
 from collections import defaultdict
 frequency = defaultdict(int)
 for text in texts:
     for token in text:
         frequency[token] += 1
->>>
+
 texts = [[token for token in text if frequency[token] > 1]
          for text in texts]
->>>
+
 from pprint import pprint  # pretty-printer
 pprint(texts)
 
