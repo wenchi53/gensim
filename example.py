a0cab00e7e33100589f004d69d6b4f5d316cc0cc
wenchi <b99901053@gmail.com>
2018-02-12 16:20:33 -0800

    Add Code.

--- example.py
+++ example.py
@@ -14,6 +14,24 @@ documents = ["Human machine interface for lab abc computer applications",
              "Graph minors A survey"]
 
 
+# remove common words and tokenize
+stoplist = set('for a of the and to in'.split())
+texts = [[word for word in document.lower().split() if word not in stoplist]
+         for document in documents]
+>>>
+# remove words that appear only once
+from collections import defaultdict
+frequency = defaultdict(int)
+for text in texts:
+    for token in text:
+        frequency[token] += 1
+>>>
+texts = [[token for token in text if frequency[token] > 1]
+         for text in texts]
+>>>
+from pprint import pprint  # pretty-printer
+pprint(texts)
+
 
 
 
